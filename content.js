04593fa80d3a4d5cf49dc70dcfa00340b71f008d
Puneet902 <puneetvaishnav902@gmail.com>
2024-11-25 21:23:53 +0530

    updated

--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.storage.local.get(["lockedSites"], ({ lockedSites }) => {
-    if (lockedSites.some((site) => window.location.href.includes(site))) {
-      document.body.innerHTML = `
-        <h1 style="color:red;text-align:center;">This website is locked!</h1>
-      `;
-    }
-  });
-  
--- readme.md
+++
@@ -1,1 +0,0 @@
-<h1>For SecureXHackathon</h1>
